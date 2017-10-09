d56e8f6a2b78187dbe82ed630d8cf48c31f77f20
Saiteja Alle <alle.saiteja@gmail.com>
2017-10-08 21:38:13 -0400

    Added Video Player View Contoller

---
+++ TheYouTubeApp/YouTubeVideoViewController.swift
@@ -0,0 +1,23 @@
+//
+//  YouTubeVideoViewController.swift
+//  TheYouTubeApp
+//
+//  Created by Saiteja Alle on 10/8/17.
+//  Copyright © 2017 Saiteja Alle. All rights reserved.
+//
+
+import UIKit
+import youtube_ios_player_helper
+
+class YouTubeVideoViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.black
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
